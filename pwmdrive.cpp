707bcfd3168b131215aefb75ceb3bb3bf8938983
boodjoom <boodjoom@gmail.com>
2016-11-14 01:58:40 +0300

    disable autoread

--- pwmdrive.cpp
+++ pwmdrive.cpp
@@ -8,16 +8,18 @@ PwmDrive::PwmDrive(uint8_t driveAddr):Drive(driveAddr)
     data->defaultValue=4095;
     data->readCode = READ_REFSPEED_FCN;
     data->writeCode = WRITE_REFSPEED_FCN;
-    data->checkAfterWrite = true;
+    data->checkAfterWrite = false;
     data->setValue(data->defaultValue);
+    data->commitChanged();
     params[(int)RefSpeed] = data;
     data = new CommData();
-    data->autoUpdate=true;
+    data->autoUpdate=false;
     data->autoWrite=false;
     data->defaultValue=4095;
     data->readCode = READ_CURRSPEED_FCN;
     data->writeCode = 0;
     data->changed=false;
+    data->checkAfterWrite = false;
     params[(int)CurSpeed] = data;
     paramIter = params;
 }
